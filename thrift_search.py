6c414e925295b486c3d48a7a4b4e73b0c43662ad
Tyler Marcus Powell <118879307+tylerpm82@users.noreply.github.com>
2025-04-20 19:09:16 -0400

    Update thrift_search.py

--- thrift_search.py
+++ thrift_search.py
@@ -166,4 +166,8 @@ def index():
 # -------------------- Run App --------------------
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
